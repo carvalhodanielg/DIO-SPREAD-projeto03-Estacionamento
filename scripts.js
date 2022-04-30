184a2424ac1e40e38688beb3cdee3e101cdad7ff
carvalhodaniel <carvalhodanielg@gmail.com>
2022-04-30 10:54:07 -0300

    local storage e métodos

--- scripts.js
+++ scripts.js
@@ -1,11 +1,42 @@
-"use strict";
-const button = document.querySelector("#button");
-const input = document.querySelector("#input");
-const item = document.querySelector("#list");
-button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
-    const task = input === null || input === void 0 ? void 0 : input.value;
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `- ${task}`;
-    item === null || item === void 0 ? void 0 : item.appendChild(listItem);
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var registrar = document.querySelector("#registrar"); //botão de registrar
+var inputNome = document.querySelector("#input-nome"); //input de nome
+var inputPlaca = document.querySelector("#input-placa"); //input de placa
+var patio = document.querySelector("#patio"); //tabela de pátio
+function adicionar(veiculo) {
+    var newCar = document.createElement('tr');
+    newCar.innerHTML = "\n    <td>".concat(veiculo.nome, "</td>\n    <td>").concat(veiculo.placa, "</td>\n    <td>").concat(veiculo.entrada, "</td>\n    <td><button class=\"delete \"data-placa=\"").concat(veiculo.placa, "\">X</button\"></td>\n    ");
+    patio === null || patio === void 0 ? void 0 : patio.appendChild(newCar);
+    salvar(__spreadArray(__spreadArray([], ler(), true), [veiculo], false));
+}
+function remover() {
+}
+function ler() {
+    return localStorage.patio ? JSON.parse(localStorage.patio) : [];
+}
+function salvar(veiculo) {
+    localStorage.setItem("patio", JSON.stringify(veiculo));
+}
+registrar === null || registrar === void 0 ? void 0 : registrar.addEventListener('click', function () {
+    console.log("add");
+    if ((inputNome === null || inputNome === void 0 ? void 0 : inputNome.value) == "" || (inputPlaca === null || inputPlaca === void 0 ? void 0 : inputPlaca.value) == "") {
+        alert("Erro!");
+        return;
+    }
+    if ((inputNome === null || inputNome === void 0 ? void 0 : inputNome.value) == null || (inputPlaca === null || inputPlaca === void 0 ? void 0 : inputPlaca.value) == null)
+        return;
+    var nome = inputNome.value;
+    var placa = inputPlaca.value;
+    var entrada = new Date().toString();
+    console.log("Nome: ".concat(nome, "; Placa: ").concat(placa));
+    console.log(new Date());
+    adicionar({ nome: nome, placa: placa, entrada: entrada });
 });
-//helo
--- scripts.ts
+++ scripts.ts
@@ -1,19 +1,63 @@
-const button = document.querySelector<HTMLInputElement>("#button")
-const input = document.querySelector<HTMLInputElement>("#input")
-const item = document.querySelector<HTMLInputElement>("#list")
+const registrar = document.querySelector<HTMLInputElement>("#registrar") //botão de registrar
+const inputNome = document.querySelector<HTMLInputElement>("#input-nome") //input de nome
+const inputPlaca = document.querySelector<HTMLInputElement>("#input-placa") //input de placa
+const patio = document.querySelector("#patio") //tabela de pátio
+
+interface Veiculo {
+    nome: string;
+    placa: string;
+    entrada: string;
+}
+
+function adicionar (veiculo: Veiculo) {
+
+    const newCar = document.createElement('tr')
+
+    newCar.innerHTML = `
+    <td>${veiculo.nome}</td>
+    <td>${veiculo.placa}</td>
+    <td>${veiculo.entrada}</td>
+    <td><button class="delete "data-placa="${veiculo.placa}">X</button"></td>
+    `
+
+    patio?.appendChild(newCar);
+
+    salvar([...ler(), veiculo])
+
+}
+
+function remover () {
+
+}
+
+function ler () {
+    return localStorage.patio ? JSON.parse(localStorage.patio) : [];
+}
+
+function salvar (veiculo: Veiculo[]) {
+    localStorage.setItem("patio", JSON.stringify(veiculo));
+
+}
 
 
-button?.addEventListener('click', ()=>{
-    const task = input?.value;
-
-    const listItem = document.createElement("li")
 
 
-    listItem.innerHTML = `- ${task}`;
+registrar?.addEventListener('click',()=>{
+    console.log("add");
+    if(inputNome?.value == "" || inputPlaca?.value ==""){
+        alert("Erro!");
+        return;
+    }
 
-    item?.appendChild(listItem)
+    if(inputNome?.value == null || inputPlaca?.value == null) return;
 
+    let nome: string = inputNome.value;
+    let placa: string = inputPlaca.value;
+    let entrada: string = new Date().toString();
 
+    console.log(`Nome: ${nome}; Placa: ${placa}`);
+    console.log(new Date());
+        
+    adicionar({nome, placa, entrada})
+    
 })
-//helo
-
